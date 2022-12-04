53f7e6da923a4ca3fd7ddfc61583d5bc7e7d2450
Nicholas Dao <nickdao86@gmail.com>
2022-12-04 14:39:38 -0800

    finally able to add markers

---
+++ api_keys.py
@@ -0,0 +1,11 @@
+# OpenWeatherMap API Key
+weather_api_key = "7d8bbe9ff84e2b42c21523f5b56d886e"
+
+# Geoapify API Key, cant render maps using geoapify
+geoapify_key = '00feae085ea4496ab07b3764acd40928'
+
+gkey = 'AIzaSyBf2K9GG5iM-qChS8_U8fpsm0lqqVuNLIU'
+
+
+
+
